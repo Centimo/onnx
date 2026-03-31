799a727fe756af17a3ac3d576448d45c4a231618
Centimo <Centimo@users.noreply.github.com>
2026-03-31 15:45:47 +0300

    conan recipe: revert abseil to 20250512.1 (required by onnxruntime FetchContent FIND_PACKAGE_ARGS)

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -53,7 +53,7 @@ class OnnxConan(ConanFile):
 
     def requirements(self):
         self.requires("protobuf/3.21.12", transitive_headers=True, transitive_libs=True)
-        self.requires("abseil/20240722.0", transitive_headers=True, transitive_libs=True)
+        self.requires("abseil/20250512.1", transitive_headers=True, transitive_libs=True)
         self.requires("zlib/1.3.1", override=True)
 
     def validate(self):
