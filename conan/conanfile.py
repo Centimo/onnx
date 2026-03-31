f6d80789720f31791257513886f49fcf23503b96
Centimo <Centimo@users.noreply.github.com>
2026-03-31 18:37:59 +0300

    conan recipe: fix cmake override to 3.28.6 (3.28.3 does not exist in ConanCenter)

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -61,7 +61,7 @@ class OnnxConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("protobuf/<host_version>")
-        self.tool_requires("cmake/3.28.3", override=True)
+        self.tool_requires("cmake/3.28.6", override=True)
 
     def source(self):
         if os.path.exists(os.path.join(self.source_folder, "CMakeLists.txt")):
