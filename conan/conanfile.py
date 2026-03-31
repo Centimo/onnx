0dbe901e3a9bde2293fe740243e3a2ef6fba9864
Centimo <Centimo@users.noreply.github.com>
2026-03-31 18:50:45 +0300

    conan recipe: tool_requires cmake/3.28.6 without modifiers

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -61,7 +61,7 @@ class OnnxConan(ConanFile):
 
     def build_requirements(self):
         self.tool_requires("protobuf/<host_version>")
-        self.tool_requires("cmake/3.28.6", force=True)
+        self.tool_requires("cmake/3.28.6")
 
     def source(self):
         if os.path.exists(os.path.join(self.source_folder, "CMakeLists.txt")):
