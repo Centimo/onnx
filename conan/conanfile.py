cfd01dbd41434ce32936e8738e11fd34f0c3b9e7
Centimo <Centimo@users.noreply.github.com>
2026-04-08 01:32:30 +0300

    conan: use full_package_mode for protobuf and abseil dependencies

--- conan/conanfile.py
+++ conan/conanfile.py
@@ -56,6 +56,10 @@ class OnnxConan(ConanFile):
         self.requires("abseil/20250512.1", transitive_headers=True, transitive_libs=True)
         self.requires("zlib/1.3.1", override=True)
 
+    def package_id(self):
+        self.info.requires["protobuf"].full_package_mode()
+        self.info.requires["abseil"].full_package_mode()
+
     def validate(self):
         check_min_cppstd(self, 17)
 
